e0de7b585491aedcbdcde005211003e3e6b6cdeb
JJTuddi <31047449+JJTuddi@users.noreply.github.com>
2023-11-12 05:16:06 +0200

    implemented the chart with the requests to DB

--- assets/js/vue/skillsChart/SkillsChartComponent.js
+++ assets/js/vue/skillsChart/SkillsChartComponent.js
@@ -1,76 +1,140 @@
-console.log("creating the app...")
-
 const app = Vue.createApp({
     template: /**/`
-<p>buna</p>
+    <div id="container_skillsRadarChart">
         <canvas id="skillsRadarChart"></canvas>
+    </div>
+<!--        <p-->
+<!--            style="margin: 5px 5px 5px 5px"-->
+<!--            :style="{color: canStrikeThrough(label) ? 'red' : 'none'}"-->
+<!--            v-for="(label, key) in availableLabels" :key="key"-->
+<!--            @click="handleLabelClick(label)"-->
+<!--        > {{ label }} </p>-->
     `,
     data() {
         return {
+            renderId: 0,
             chartData: {
                 labels: [],
                 datasets: [],
-            }
+            },
+            rawData: {
+                labels: [],
+                datasets: [],
+            },
+            availableLabels: [],
+            selectedLabels: [],
         }
     },
     computed: {
 
     },
     methods: {
+        handleLabelClick(label) {
+            console.log("clicked on: " + label)
+            console.log(this.selectedLabels)
+            if (this.selectedLabels.find(e => e === label)) {
+                this.selectedLabels = this.selectedLabels.filter(e => e !== label);
+            } else {
+                this.selectedLabels = [...this.selectedLabels, label];
+            }
+            this.chartData = {
+                ...this.chartData
+            };
+        },
+        canStrikeThrough(label) {
+            return !!this.selectedLabels.find(e => e === label);
+        },
+        extract(skillsHistory) {
+            const sortComparator = (e1, e2) => e1['domain'] > e2['domain'];
+            const result = {}
+            let domains = new Set();
+            for (let i = 0; i < skillsHistory.length; i++) {
+                const skillHist = skillsHistory[i];
+                if (!result[skillHist['moment']]) {
+                    result[skillHist['moment']] = [];
+                }
+                result[skillHist['moment']].push(skillHist);
+                domains.add(skillHist['domain']);
+            }
+            domains = [...domains].sort(sortComparator);
+            const keys = Object.keys(result);
+            for (let i = 0; i < keys.length; i++) {
+                const moment = keys[i];
+                result[moment] = result[moment].sort(sortComparator);
+            }
+            return {
+                labels: domains,
+                datasets: [...Object.entries(result).map(
+                    entry => ({
+                        label: entry[0],
+                        data: entry[1].map(val => +val['theoreticalScore'] + +val['practicalScore'])
+                    })
+                )]
+            };
+        },
+        fetchSkills() {
+            const requestOptions = {
+                method: 'GET',
+                redirect: 'follow',
+                headers: {
+                    'Authorization': `Bearer ${getCookie('authCookie')}`
+                }
+            };
 
+            fetch("http://{{BASE_HOST}}/api/skills".replace("{{BASE_HOST}}", baseHost), requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    const newChartData = this.extract(result);
+                    this.availableLabels = newChartData['labels'];
+                    this.selectedLabels = ['game developer',
+                        'data science',
+                        'machine learning',
+                        'quality assurance',
+                        'devops',
+                        'cybersecurity',
+                        'database administrator',
+                        // 'mobile developer',
+                        'frontend'];
+                    this.chartData = newChartData;
+                });
+        },
+        removeSelected(chartData) {
+            const removeLabel = this.availableLabels.map(label => this.selectedLabels.indexOf(label) === -1)
+            const removed =  {
+                labels: chartData['labels'].filter((_, indx) => removeLabel[indx]),
+                datasets: chartData['datasets'].map(dataset => ({
+                    ...dataset,
+                    data: dataset['data'].filter((_, indx) => removeLabel[indx])
+                }))
+            }
+            return removed;
+        }
     },
     watch: {
-        chartData(newValue, oldValue) {
-            console.log("new data setting..." + JSON.stringify(newValue))
-            const ctx = document.getElementById('skillsRadarChart');
+        chartData(newValue) {
+            // document.getElementById("container_skillsRadarChart").innerHTML = "";
+            // document.getElementById("container_skillsRadarChart").innerHTML += `<canvas id="skillsRadarChart${this.renderId++}"></canvas>`;
+            const ctx = document.getElementById(`skillsRadarChart`);
 
-            new Chart(ctx, {
-                type: 'radar',
-                data: newValue,
-                options: {
-                    elements: {
-                        line: {
-                            borderWidth: 3
+            const newData = this.removeSelected(newValue);
+
+            setTimeout(() => {
+                new Chart(ctx, {
+                    type: 'radar',
+                    data: newData,
+                    options: {
+                        elements: {
+                            line: {
+                                borderWidth: 3
+                            }
                         }
-                    }
-                },
-            });
+                    },
+                });
+            }, 100);
         }
     },
     mounted() {
-        console.log("mounting...")
-        this.chartData = {
-            labels: [
-                'Eating',
-                'Drinking',
-                'Sleeping',
-                'Designing',
-                'Coding',
-                'Cycling',
-                'Running'
-            ],
-            datasets: [{
-                label: 'My First Dataset',
-                data: [65, 59, 90, 81, 56, 55, 40],
-                fill: true,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgb(255, 99, 132)',
-                pointBackgroundColor: 'rgb(255, 99, 132)',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
-                pointHoverBorderColor: 'rgb(255, 99, 132)'
-            }, {
-                label: 'My Second Dataset',
-                data: [28, 48, 40, 19, 96, 27, 100],
-                fill: true,
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgb(54, 162, 235)',
-                pointBackgroundColor: 'rgb(54, 162, 235)',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
-                pointHoverBorderColor: 'rgb(54, 162, 235)'
-            }]
-        };
+        this.fetchSkills();
     }
 });
 
